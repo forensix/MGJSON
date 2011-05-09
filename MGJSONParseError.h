f7eb5480fec3a2bf4230571ad36ef7f13c7c7d53
Manuel Gebele <forensixs@gmx.de>
2011-05-09 15:25:10 +0200

    Initial commit

--- MGJSONParseError.h
+++ MGJSONParseError.h
@@ -2,7 +2,7 @@
 //  MGJSONParseError.h
 //  MGJSON
 //
-//  MGAOP is available under *either* the terms of the modified BSD license
+//  MGJSON is available under *either* the terms of the modified BSD license
 //  *or* the MIT License (2008). See http://opensource.org/licenses/alphabetical
 //  for full text.
 // 
